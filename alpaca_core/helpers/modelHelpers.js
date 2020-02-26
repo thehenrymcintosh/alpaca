7b8c93e1cda5075bd6571874eb1210ac339b3ce4
Henry McIntosh <henry.mcintosh@roardigital.co.uk>
2020-02-26 15:48:11 +0000

    slight refactor

---
+++ alpaca_core/helpers/modelHelpers.js
@@ -0,0 +1,55 @@
+const { AlpacaArray, AlpacaDate, AlpacaType } = require("../types/_index");
+const validators = require("./validators");
+
+const primitiveToString = ( primitive ) => {
+  if ( validators.isValidFunction( primitive ) ) {
+    if ( primitive.name === "ObjectId" ){
+      return "string";
+    } else {
+      return primitive.name;
+    }
+  } 
+  if ( validators.isValidText( primitive ) ) return primitive;
+  throw new Error(`Cannot cast primitive "${primitive}" to string!`)
+}
+
+const extractType = ( mixedInput ) => {
+  if ( mixedInput instanceof AlpacaType ) {
+    return {
+      isAlpacaArray: false,
+      type: mixedInput,
+      rawObject: null,
+      typeOrArray: mixedInput,
+    }
+  } else if ( mixedInput instanceof AlpacaArray ) {
+    return {
+      isAlpacaArray: true,
+      type: mixedInput.type,
+      rawObject: null,
+      typeOrArray: mixedInput,
+    }
+  } else if ( validators.isValidObject( mixedInput ) && ( mixedInput.type instanceof AlpacaArray || mixedInput.type instanceof AlpacaType ) ) {
+    if ( mixedInput.type instanceof AlpacaType ) {
+      return {
+        isAlpacaArray: false,
+        type: mixedInput.type,
+        rawObject: mixedInput,
+        typeOrArray: mixedInput.type,
+      }
+    } else if ( mixedInput.type instanceof AlpacaArray ) {
+      return {
+        isAlpacaArray: true,
+        type: mixedInput.type.type,
+        rawObject: mixedInput,
+        typeOrArray: mixedInput.type,
+      }
+    }
+  } else {
+    throw new Error("Not AlpacaType or AlpacaArray, and also not an object with .type of type AlpacaType or AlpacaArray!");
+  }
+}
+
+module.exports = {
+  primitiveToString,
+  extractType,
+}
--- alpaca_core/model.js
+++ alpaca_core/model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require( "express" );
+const { primitiveToString, extractType } = require("./helpers/modelHelpers");
 
 const { AlpacaArray, AlpacaDate, AlpacaType } = require("./types/_index");
 
@@ -15,54 +16,6 @@ const CreatedAt = new AlpacaDate();
 
 function getModelByName( name ) {
   return GlobalModelStore[ name.toLowerCase() ];
-}
-
-const primitiveToString = ( primitive ) => {
-  if ( validators.isValidFunction( primitive ) ) {
-    if ( primitive.name === "ObjectId" ){
-      return "string";
-    } else {
-      return primitive.name;
-    }
-  } 
-  if ( validators.isValidText( primitive ) ) return primitive;
-  throw new Error(`Cannot cast primitive "${primitive}" to string!`)
-}
-
-const extractType = ( mixedInput ) => {
-  if ( mixedInput instanceof AlpacaType ) {
-    return {
-      isAlpacaArray: false,
-      type: mixedInput,
-      rawObject: null,
-      typeOrArray: mixedInput,
-    }
-  } else if ( mixedInput instanceof AlpacaArray ) {
-    return {
-      isAlpacaArray: true,
-      type: mixedInput.type,
-      rawObject: null,
-      typeOrArray: mixedInput,
-    }
-  } else if ( validators.isValidObject( mixedInput ) && ( mixedInput.type instanceof AlpacaArray || mixedInput.type instanceof AlpacaType ) ) {
-    if ( mixedInput.type instanceof AlpacaType ) {
-      return {
-        isAlpacaArray: false,
-        type: mixedInput.type,
-        rawObject: mixedInput,
-        typeOrArray: mixedInput.type,
-      }
-    } else if ( mixedInput.type instanceof AlpacaArray ) {
-      return {
-        isAlpacaArray: true,
-        type: mixedInput.type.type,
-        rawObject: mixedInput,
-        typeOrArray: mixedInput.type,
-      }
-    }
-  } else {
-    throw new Error("Not AlpacaType or AlpacaArray, and also not an object with .type of type AlpacaType or AlpacaArray!");
-  }
 }
 
 class AlpacaModel {
