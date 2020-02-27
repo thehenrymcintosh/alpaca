a2d44e08a34692bba6348626d3b91e0782a7d0b9
Henry McIntosh <henry.mcintosh@roardigital.co.uk>
2020-02-27 20:23:52 +0000

    export validators

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,5 +9,22 @@ declare const _default: {
         AlpacaDate: typeof import("./types/date").default;
         AlpacaType: typeof import("./types/type").default;
     };
+    AlpacaValidators: {
+        isValidArray: (arr: any) => boolean;
+        isValidBool: (bool: any) => boolean;
+        isValidDate: (date: any) => boolean;
+        isValidEmail: (email: any) => boolean;
+        isValidIdString: (testId: any) => boolean;
+        isValidId: (testId: any) => boolean;
+        isValidInt: (num: any) => boolean;
+        isValidName: (name: any) => boolean;
+        isValidNumber: (num: any) => boolean;
+        isValidUrl: (url: any) => boolean;
+        isValidText: (text: any) => boolean;
+        isValidObject: (obj: any) => boolean;
+        getValidateMethodForType: (type: string) => (name: any) => boolean;
+        findInvalidIds: (ids: any) => any[];
+        isValidFunction: (func: any) => boolean;
+    };
 };
 export = _default;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Model from "./model";
 import Types from "./types/_index";
+import Validators from "./helpers/validators";
 
 export = {
   AlpacaModel: Model,
   AlpacaTypes: Types,
+  AlpacaValidators: Validators,
 };
